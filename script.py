3ccf9a14c89b62d6d5a559e003764d2e0600c2f8
jimmymcmahoncoding <78510705+jimmymcmahoncoding@users.noreply.github.com>
2022-04-08 11:42:54 +0100

    Accepting merge conflict.

--- script.py
+++ script.py
@@ -9,13 +9,10 @@ print(sys.executable)
 
 test_variable = 1
 
-<<<<<<< HEAD
-
 test_vs_code = "I am an edit from VS Code."
 
-=======
 test_browser = "I am a new edit from the browser"
->>>>>>> eb3ed8c63f550478ab3bf55441db56764cc54dc8
+
 
 def greet(who_to_greet):
     greeting = f"Hello, {who_to_greet}"
