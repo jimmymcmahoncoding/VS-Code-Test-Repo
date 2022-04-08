894decda5003edf774e51c3ea113c086d62860ed
jimmymcmahoncoding <78510705+jimmymcmahoncoding@users.noreply.github.com>
2022-04-08 11:18:32 +0100

    Removed some code

--- script.py
+++ script.py
@@ -1,6 +1,4 @@
-import math
 import os
-import sys
 from os import renames
 
 
@@ -13,12 +11,6 @@ print(sys.executable)
 def greet(who_to_greet):
     greeting = f"Hello, {who_to_greet}"
     return greeting
-
-
-print(greet("World"))
-print(greet("Jimmy"))
-r = requests.get("https://www.google.com")
-print(r.status_code)
 
 
 name = input("Your Name?")
